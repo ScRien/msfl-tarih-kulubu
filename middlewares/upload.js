30c9515a59dcefefeb2f05bbbcb83f75f69a7071
ScRien <sah.fdn25@gmail.com>
2025-11-16 15:13:13 +0300

    upload fixed

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,13 +1,29 @@
 import multer from "multer";
+import fs from "fs";
 import path from "path";
 
-// TEMP klasörüne kaydediyoruz
+// TMP klasörü yoksa otomatik oluştur
+const uploadDir = "tmp";
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+// Güvenli dosya adı oluşturma fonksiyonu
+function sanitizeFilename(filename) {
+  return filename
+    .normalize("NFD") // unicode normalize
+    .replace(/[\u0300-\u036f]/g, "") // aksan temizle
+    .replace(/[^a-zA-Z0-9._-]/g, "_"); // özel karakterleri _ yap
+}
+
+// Multer storage config
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "tmp/"); // tmp klasörü oluşturulacak
+    cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
-    const uniqueName = Date.now() + "-" + file.originalname;
+    const safeName = sanitizeFilename(file.originalname);
+    const uniqueName = Date.now() + "-" + safeName;
     cb(null, uniqueName);
   },
 });
